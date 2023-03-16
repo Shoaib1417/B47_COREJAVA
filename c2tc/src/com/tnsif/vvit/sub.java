849edfe9aa6da1f401c6e40814237c23717b17f1
Shoaib Ahmed <Shoaib Ahmed@DESKTOP-8IQ6GOR>
2023-03-16 12:10:18 +0530

    subtraction

---
+++ c2tc/src/com/tnsif/vvit/sub.java
@@ -0,0 +1,12 @@
+package com.tnsif.vvit;
+
+public class sub {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int a=240;
+		int b=200;
+		System.out.println(a-b);
+	}
+
+}
